023012980a0a1e247744abac2d12fa0b24b697d7
synthmax <max@koblo.com>
2011-06-12 22:57:56 +0200

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,11 @@
 #  Page.find_or_create_by_identity(identity)
 #end
 
-#User.create(:email => "someone@synthmax.dk", :password => "someone1234", :role => "super", :name => "Someone", :language => "en")
+User.create(:email => "max@synthmax.dk", :password => "rosalina", :role => "super", :name => "Max Grønlund", :language => "dk")
 
-Page.create(:identity => "welcome_dk" , :title => 'Velkommen', :body => 'formålet med denne side kan beskrives her', :language => "dk")
-Page.create(:identity => "welcome_en" , :title => 'Welcome', :body => 'The purpose of this site can be explained here', :language => "en")
-Page.create(:identity => "about_dk" , :title => 'Om denne side', :body => 'En forklaring af, sidens forfattere, formål e.t.c.', :language => "dk")
-Page.create(:identity => "about_en" , :title => 'About this site', :body => 'An explanation of the site. The authors. and the values', :language => "en")
-
-Page.create(:identity => "history_dk" , :title => 'Historie', :body => 'Århus Kommune købte ejendommen i 1986...', :language => "dk")
+#Page.create(:identity => "welcome_dk" , :title => 'Velkommen', :body => 'formålet med denne side kan beskrives her', :language => "dk")
+#Page.create(:identity => "welcome_en" , :title => 'Welcome', :body => 'The purpose of this site can be explained here', :language => "en")
+#Page.create(:identity => "about_dk" , :title => 'Om denne side', :body => 'En forklaring af, sidens forfattere, formål e.t.c.', :language => "dk")
+#Page.create(:identity => "about_en" , :title => 'About this site', :body => 'An explanation of the site. The authors. and the values', :language => "en")
+#
+#Page.create(:identity => "history_dk" , :title => 'Historie', :body => 'Århus Kommune købte ejendommen i 1986...', :language => "dk")
