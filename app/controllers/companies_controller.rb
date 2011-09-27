c73fadb27dfff310994a761838eda308d39e686b
Synthmax <max@synthmax.dk>
2011-09-27 12:16:47 +0200

    -

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,6 +44,9 @@ class CompaniesController < InheritedResources::Base
   
   def show
     @link = '<a href="http://'+@company.homepage+'">'+@company.homepage+'<a/>'
+    if user_signed_in?
+      @newsposts = Newspost.internal.order('created_at desc').paginate(:per_page => 3, :page => params[:page])
+    end
   end
 
 
