ab6c95e18b999213a1aa70013bc825d2efe1b2dc
Synthmax <max@synthmax.dk>
2011-10-07 17:40:29 +0200

    minor stuff css

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails_template::Application.config.session_store :cookie_store, :key => '_officelab_session'
+Rails_template::Application.config.session_store :cookie_store, :key => '_mst4_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
