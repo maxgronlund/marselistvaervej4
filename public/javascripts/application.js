49704804de9f8108c53b9a9568457f2c0853f413
Lars Hoeg <larshoeg@gmail.com>
2011-10-07 13:37:30 +0200

    retake of limiting initial size of company info div

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-	$('div.demo-show:eq(0)> div').hide();  
-	  $('div.demo-show:eq(0)> unfold').click(function() {
+	$('div.company-info .limited').height('160px');
+	$('div.company-info .unfold').click(function() {
 		$(this).hide();
-	    $(this).next().slideToggle('fast');
-	  });
+		$(this).prev('.limited').css('height', null);
+	});
     $('.comment_reply').hide();
     $('.comment_reply').before('<a href="#" class="comment_reply_link">svar</a>');
 
