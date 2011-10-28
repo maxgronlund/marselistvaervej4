dde457f979d30563a7aaa107ef954f3c5fe385c1
Synthmax <max@synthmax.dk>
2011-10-28 14:13:43 +0200

    default illustration

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -2,7 +2,7 @@
 class Illustration < ActiveRecord::Base
 
   
-  has_attached_file :image, :styles => {  :mini => "32x32", :thumbnail => "240x240" }, :default_url => "/images/fallback/default_illustration_:style.gif"
+  has_attached_file :image, :styles => {  :mini => "32x32", :thumbnail => "240x240" }, :default_url => "/images/fallback/default_illustration_:style.jpg"
   
   validates_attachment_size :image, :less_than => 800.kilobytes
 #  validates_attachment_content_type :image, :content_type => ['image/jpeg','image/jpg','image/png','image/x-png','image/gif','image/pjpeg']
