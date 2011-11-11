a32f0fd7c5a27c4c4fdfffe988a83fb350126d84
Jørgen Orehøj Erichsen <joe@erichsen.net>
2011-11-11 15:42:43 +0100

    we try again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,10 @@ Rails_template::Application.config.session_store :cookie_store, :key => '_mst4_s
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Rails_template::Application.config.session_store :active_record_store
+=begin
 Rails.application.config.middleware.insert_before(
   ActionDispatch::Session::CookieStore,
+  FlashSessionCookieMiddleware,
   Rails.application.config.session_options[:key]
 )
+=end
